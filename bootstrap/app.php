640bdbeb9796a1eb3211fc6b76887b06b5ccedff
pantheoncore <chris@pantheoncore.com>
2025-02-19 22:46:57 -0500

    update to routing

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,16 +3,28 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
+use Laravel\Sanctum\Http\Middleware\CheckAbilities;
+use Laravel\Sanctum\Http\Middleware\CheckForAnyAbility;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        using: function() {
+            Route::middleware('api')
+            ->prefix('api')
+            ->group(base_path('routes/api.php'));
+ 
+            Route::middleware('web')
+            ->group(base_path('routes/web.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'abilities' => CheckAbilities::class,
+            'ability' => CheckForAnyAbility::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
